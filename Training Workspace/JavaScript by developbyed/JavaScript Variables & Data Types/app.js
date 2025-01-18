0b32bad821deba638009944971690d752d2ee11a
BCABELLO-MYNBCC <BCABELLO01@mynbcc.ca>
2025-01-18 16:11:16 -0400

    (JAN 18) Changes in Windows OS

---
+++ Training Workspace/JavaScript by developbyed/JavaScript Variables & Data Types/app.js
@@ -0,0 +1,18 @@
+var life = 100; // This is my life bar NUMBER
+life = 50; // Change the value of LIFE VARIABLE
+life = life - 10; // 40
+life = life * 10; // 400
+
+var name = "Siri"; // This is the name displayed in the game STRING
+
+/* BOOLEAN */
+var checkout = true;
+
+var box; // undefined
+
+console.log("The sum of 40 multiply by 10 is " + life); // Display
+console.log("Example of: " + box);
+
+/* NEW VARIABLES */
+const sample = 100; // CONST Variable cannot be declared or change the value
+let num = 50;
